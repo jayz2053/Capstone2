bbbc8bd156d8aa390fc0fa12d44530616f9961a4
jayz2053 <jpj19@students.uwf.edu>
2018-04-27 19:35:12 -0500

    update functions

--- app/routes.py
+++ app/routes.py
@@ -18,6 +18,6 @@ def getProfessors():
 
 @app.route('/professor/<string:email_id>', methods=["GET"])
 def getProfessor(email_id):
-    uno_prof = Professor.query.filter(email == '{}@uwf.edu'.format(email_id))
+    uno_prof = Professor.query.filter(Professor.email == '{}@uwf.edu'.format(email_id))
     result = professorSchema.dump(uno_prof)
     return jsonfiy(results.data)
